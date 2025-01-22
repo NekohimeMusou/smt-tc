ad6b398a20365cb43b9097ad2304b50d4f1ebf5b
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-01-22 15:44:38 -0500

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { arguments: false } },
+      ],
+    },
+  },
+  {
+    ignores: ["eslint.config.mjs"],
+  },
+);
