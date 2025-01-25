5dd0a94bd43329fef8a25d3f914501d2a04cd858
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-01-24 20:31:16 -0500

    Finish attack class

--- src/module/data-models/item/abstract/attack.ts
+++ src/module/data-models/item/abstract/attack.ts
@@ -5,7 +5,32 @@ import { SmtBaseItemData } from "./base.js";
 export abstract class AttackData extends SmtBaseItemData {
   declare type: "skill" | "weapon";
 
-  get accuracy(): number {
+  get pierce(): boolean {
+    const data = this._systemData;
+    const actor = this.parent?.parent as SmtActor;
+
+    return data.affinity === "phys" && actor.system.pierce;
+  }
+
+  get damageType(): DamageType {
+    const data = this._systemData;
+
+    return data.skillType === "phys" ? "phys" : "mag";
+  }
+
+  // TODO: Add "item" type
+  get powerBoost(): boolean {
+    const data = this._systemData;
+
+    const boostType: PowerBoostType =
+      data.skillType === "phys" ? "phys" : "mag";
+
+    const actor = this.parent?.parent as SmtActor;
+
+    return actor.system.powerBoost[boostType];
+  }
+
+  get tn(): number {
     const data = this._systemData;
     const actor = this.parent?.parent as SmtActor;
 
@@ -30,24 +55,38 @@ export abstract class AttackData extends SmtBaseItemData {
         return 100;
     }
 
-    return actor.system.tn[tn];
+    return tn === "negotiation"
+      ? actor.system.tn[tn] + 20
+      : actor.system.tn[tn];
   }
 
-  get pierce(): boolean {
+  get power(): number {
+    const actor = this.parent?.parent as SmtActor;
     const data = this._systemData;
+
+    // if (data.itemType === "gun") {
+    //   // TODO: Return gun power
+    // }
+
+    const powerType = data.skillType === "phys" ? "phys" : "mag";
+
+    return actor.system.power[powerType];
+  }
+
+  get autoFailThreshold(): number {
     const actor = this.parent?.parent as SmtActor;
 
-    return data.affinity === "phys" && actor.system.pierce;
+    return actor.system.autoFailThreshold;
   }
 
-  get damageType(): DamageType {
+  get critBoost(): boolean {
+    const actor = this.parent?.parent as SmtActor;
     const data = this._systemData;
 
-    return data.skillType === "phys" ? "phys" : "mag";
+    return (
+      data.innateCritBoost || (data.damageType === "phys" && actor.system.might)
+    );
   }
-
-  // get powerBoost(): boolean {
-  // }
 
   static override defineSchema() {
     const fields = foundry.data.fields;
