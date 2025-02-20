53adce5e6148956b5648e4751c1b338c6348765a
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-02-20 15:13:23 -0500

    move up base

--- src/module/data-models/actor/abstract/base.ts
+++ src/module/data-models/actor/abstract/base.ts
@@ -1,5 +1,5 @@
 import { SmtActor } from "../../../documents/actor/actor.js";
-import baseSchemaFields from "./base-schema-fields.js";
+import { DefenseAffinityData } from "../../defense-affinities.js";
 
 export abstract class SmtBaseActorData extends foundry.abstract.TypeDataModel {
   abstract readonly type: CharacterClass;
@@ -26,8 +26,141 @@ export abstract class SmtBaseActorData extends foundry.abstract.TypeDataModel {
   }
 
   static override defineSchema() {
+    const fields = foundry.data.fields;
+
+    const stats = new fields.SchemaField({
+      st: new fields.SchemaField({
+        ...generateStatSchema(),
+        derivedTN: new fields.StringField({ initial: "phys" }),
+      }),
+      ma: new fields.SchemaField({
+        ...generateStatSchema(),
+        derivedTN: new fields.StringField({ initial: "mag" }),
+      }),
+      vi: new fields.SchemaField({
+        ...generateStatSchema(),
+        derivedTN: new fields.StringField({ initial: "save" }),
+      }),
+      ag: new fields.SchemaField({
+        ...generateStatSchema(),
+        derivedTN: new fields.StringField({ initial: "dodge" }),
+      }),
+      lu: new fields.SchemaField({
+        ...generateStatSchema(),
+        derivedTN: new fields.StringField({ initial: "negotiation" }),
+      }),
+    });
+
+    const resources = {
+      hp: new fields.SchemaField({
+        max: new fields.NumberField({ integer: true }),
+        value: new fields.NumberField({ integer: true }),
+      }),
+      mp: new fields.SchemaField({
+        max: new fields.NumberField({ integer: true }),
+        value: new fields.NumberField({ integer: true }),
+      }),
+      fp: new fields.SchemaField({
+        max: new fields.NumberField({ integer: true }),
+        value: new fields.NumberField({ integer: true }),
+      }),
+    };
+
+    const tn = new fields.SchemaField({
+      st: new fields.NumberField({ integer: true }),
+      ma: new fields.NumberField({ integer: true }),
+      vi: new fields.NumberField({ integer: true }),
+      ag: new fields.NumberField({ integer: true }),
+      lu: new fields.NumberField({ integer: true }),
+      phys: new fields.NumberField({ integer: true }),
+      mag: new fields.NumberField({ integer: true }),
+      save: new fields.NumberField({ integer: true }),
+      dodge: new fields.NumberField({ integer: true }),
+      negotiation: new fields.NumberField({ integer: true }),
+    });
+
+    const power = new fields.SchemaField({
+      phys: new fields.NumberField({ integer: true, min: 0 }),
+      mag: new fields.NumberField({ integer: true, min: 0 }),
+      gun: new fields.NumberField({ integer: true, min: 0 }),
+    });
+
+    const resist = new fields.SchemaField({
+      phys: new fields.NumberField({ integer: true, min: 0 }),
+      mag: new fields.NumberField({ integer: true, min: 0 }),
+    });
+
+    const bioData = {
+      notes: new fields.HTMLField(),
+      backgrounds: new fields.ArrayField(new fields.StringField()),
+      contacts: new fields.ArrayField(new fields.StringField()),
+      bond: new fields.StringField(),
+      goal: new fields.StringField(),
+      alignment: new fields.SchemaField({
+        law: new fields.NumberField({ integer: true, min: 0 }),
+        chaos: new fields.NumberField({ integer: true, min: 0 }),
+        dark: new fields.NumberField({ integer: true, min: 0 }),
+        light: new fields.NumberField({ integer: true, min: 0 }),
+        neutral: new fields.NumberField({ integer: true, min: 0 }),
+        heeHo: new fields.NumberField({ integer: true, min: 0 }),
+      }),
+      behavior: new fields.StringField(),
+    };
+
+    const awards = new fields.SchemaField({
+      xp: new fields.NumberField({ integer: true, min: 0 }),
+      macca: new fields.NumberField({ integer: true, min: 0 }),
+      itemDrops: new fields.StringField(),
+    });
+
+    const sheetData = {
+      tnBoosts: new fields.NumberField({ integer: true, initial: 0 }),
+      multi: new fields.NumberField({
+        integer: true,
+        initial: 1,
+        positive: true,
+        max: 3,
+      }),
+    };
+    const buffs = new fields.SchemaField({
+      tarukaja: new fields.NumberField({ integer: true, min: 0 }),
+      makakaja: new fields.NumberField({ integer: true, min: 0 }),
+      rakukaja: new fields.NumberField({ integer: true, min: 0 }),
+      sukukaja: new fields.NumberField({ integer: true, min: 0 }),
+      tarunda: new fields.NumberField({ integer: true, min: 0 }),
+      rakunda: new fields.NumberField({ integer: true, min: 0 }),
+      sukunda: new fields.NumberField({ integer: true, min: 0 }),
+    });
+    // Modifiers for AEs etc to latch onto
+    const mods = new fields.SchemaField({
+      might: new fields.BooleanField(),
+    });
+
+    const powerBoost = new fields.SchemaField({
+      phys: new fields.BooleanField(),
+      mag: new fields.BooleanField(),
+      item: new fields.BooleanField(),
+    });
+
     return {
-      ...baseSchemaFields(),
+      stats,
+      ...resources,
+      tn,
+      power,
+      resist,
+      ...bioData,
+      awards,
+      ...sheetData,
+      buffs,
+      mods,
+      powerBoost,
+      hpMultiplier: new fields.NumberField({ integer: true, positive: true }),
+      mpMultiplier: new fields.NumberField({ integer: true, positive: true }),
+      xp: new fields.NumberField({ integer: true, min: 0 }),
+      macca: new fields.NumberField({ integer: true, min: 0 }),
+      affinities: new fields.EmbeddedDataField(DefenseAffinityData),
+      // Should this be considered an NPC for e.g. Auto Dodge
+      npc: new fields.BooleanField(),
     } as const;
   }
 
@@ -112,4 +245,15 @@ export abstract class SmtBaseActorData extends foundry.abstract.TypeDataModel {
   protected get _systemData() {
     return this as this & Subtype<SmtActor, this["type"]>["system"];
   }
+}
+
+function generateStatSchema() {
+  const fields = foundry.data.fields;
+
+  return {
+    base: new fields.NumberField({ integer: true, initial: 1, min: 1 }),
+    lv: new fields.NumberField({ integer: true, min: 0 }),
+    mgt: new fields.NumberField({ integer: true, min: 0 }),
+    value: new fields.NumberField({ integer: true, min: 1 }),
+  };
 }
