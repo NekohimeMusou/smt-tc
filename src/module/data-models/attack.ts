ee93dc9e10dde3852b5168dab7cde33efbc8d0a3
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-03-05 21:42:14 -0500

    Partial attack DM

--- src/module/data-models/attack.ts
+++ src/module/data-models/attack.ts
@@ -5,10 +5,24 @@ export default class AttackDataModel extends foundry.abstract.DataModel {
     return {
       auto: new fields.BooleanField(),
       attackType: new fields.StringField({
-        choices: CONFIG.SMT.attackTypes,
+        choices: {
+          ...CONFIG.SMT.attackTypes,
+          item: "SMT.attackTypes.item",
+        },
         initial: "phys",
       }),
       potency: new fields.NumberField({ integer: true, min: 0 }),
+      affinity: new fields.StringField({
+        choices: {
+          ...CONFIG.SMT.attackAffinities,
+          talk: "SMT.affinities.talk",
+        },
+        initial: "phys",
+      }),
+      target: new fields.StringField({
+        choices: CONFIG.SMT.targets,
+        initial: "one",
+      }),
     };
   }
 }
