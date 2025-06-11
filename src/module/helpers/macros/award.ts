a46f95a16c793b53709903484540a008565ed797
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-06-11 00:01:51 -0400

    Combine xp/macca award and Lucky Find macros

--- src/module/helpers/macros/award.ts
+++ src/module/helpers/macros/award.ts
@@ -126,23 +126,26 @@ export async function grantRewards() {
     },
   };
 
-  return await ChatMessage.create(chatData);
+  await ChatMessage.create(chatData);
+
+  // Do Lucky Find checks
+  await luckyFindCheck(tokens);
 }
 
-export async function luckyFindCheck() {
+export async function luckyFindCheck(tokens: SmtToken[]) {
   // GMs only!
   if (!game.user.isGM) {
     return ui.notifications.notify(game.i18n.localize("SMT.ui.gmOnly"));
   }
 
-  const tokens = canvas.tokens.controlled as SmtToken[];
+  // const tokens = canvas.tokens.controlled as SmtToken[];
 
   // If no tokens are controlled, display a notification
-  if (tokens.length < 1) {
-    return ui.notifications.notify(
-      game.i18n.localize("SMT.ui.noTokensSelected"),
-    );
-  }
+  // if (tokens.length < 1) {
+  //   return ui.notifications.notify(
+  //     game.i18n.localize("SMT.ui.noTokensSelected"),
+  //   );
+  // }
 
   const results = await Promise.all(
     tokens
